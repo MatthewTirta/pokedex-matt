223c590b6f980a867dc13fa07f58fc7d0650378c
MatthewTirta <tirtamatt@gmail.com>
2023-09-13 15:51:04 +0700

    Update Search bar and Type Border

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,27 @@ const capitalize = (str) => {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
+const typeColors = {
+	fire: '#ff6849',
+	water: '#59aeff',
+	grass: '#93d776',
+	poison: '#bc76ae',
+	flying: '#85aeff',
+	bug: '#bcc949',
+	normal: '#c9c9bd',
+	electric: '#ffd759',
+	ground: '#e5c976',
+	fairy: '#ffbdff',
+	fighting: '#c97668',
+	psychic: '#ff76ae',
+	rock: '#c9bd85',
+	steel: '#bcbdc9',
+	ice: '#93e5ff',
+	ghost: '#8585c9',
+	dark: '#937668',
+	dragon: '#9385f2',
+};
+
 const Pokemon = ({ species, Index, types }) => {
 	const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
 		Index + 1
@@ -22,7 +43,23 @@ const Pokemon = ({ species, Index, types }) => {
 			<div className="pokemon-info">
 				<p className="pokemon-id"># {Index + 1}</p>
 				<p className="pokemon-name">{capitalize(species.name)}</p>
-				<p className="pokemon-type">Type: {types.join(', ')}</p>
+				<p className="pokemon-type">
+					{types.map((type) => (
+						<span
+							key={type}
+							style={{
+								backgroundColor: typeColors[type],
+								color: 'black',
+								padding: '2px 5px',
+								margin: '0 5px',
+								borderRadius: '5px',
+								display: 'inline-block',
+							}}
+						>
+							{capitalize(type)}
+						</span>
+					))}
+				</p>
 			</div>
 		</div>
 	);
@@ -51,8 +88,11 @@ const PokemonHome = () => {
 		});
 	}, []);
 
-	const filteredPokemon = pokemon.filter((species) =>
-		species.name.includes(search.toLowerCase())
+	const filteredPokemon = pokemon.filter(
+		(species) =>
+			species.name.includes(search.toLowerCase()) ||
+			(Types[species.name] &&
+				Types[species.name].some((type) => type.includes(search.toLowerCase())))
 	);
 
 	return (
@@ -60,7 +100,7 @@ const PokemonHome = () => {
 			<div className="search-bar">
 				<Input
 					type="text"
-					placeholder="Search Pokémon by name"
+					placeholder="Search Pokémon by name / type"
 					value={search}
 					onChange={(e) => setSearch(e.target.value)}
 				/>
@@ -79,6 +119,130 @@ const PokemonHome = () => {
 					);
 				})}
 			</div>
+		</Container>
+	);
+};
+
+const LiveCode = () => {
+	const [users, setUsers] = useState([]);
+	const [search, setSearch] = useState('');
+	const [gender, setGender] = useState('ALL');
+	const [age, setAge] = useState('norm');
+
+	useEffect(() => {
+		const apiURL = 'https://dummyjson.com/users';
+		fetch(apiURL)
+			.then((data) => {
+				data.json().then((data) => setUsers(data.users));
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, []);
+
+	const filterUser = users.filter(
+		(user) =>
+			(gender === 'ALL' || user.gender === gender) &&
+			`${user.firstName} ${user.lastName}`
+				.toLowerCase()
+				.includes(search.toLowerCase())
+	);
+
+	const sortedUsers = filterUser.sort((a, b) => {
+		if (age === 'asc') {
+			return a.age - b.age;
+		} else if (age === 'desc') {
+			return b.age - a.age;
+		} else {
+			return 0;
+		}
+	});
+
+	const listGender = (e) => {
+		setGender(e.target.value);
+	};
+
+	const genderMask = (gender) => {
+		if (gender === 'male') {
+			return 'M';
+		} else if (gender === 'female') {
+			return 'F';
+		}
+	};
+
+	const delUser = (userId) => {
+		setUsers((prevUsers) => prevUsers.filter((user) => user.id !== userId));
+	};
+
+	return (
+		<Container>
+			<div>
+				<h1>DATA</h1>
+			</div>
+			<div>
+				<input
+					type="text"
+					placeholder="Search by Name"
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+				<select
+					value={gender}
+					onChange={listGender}
+					style={{ marginLeft: '10px' }}
+				>
+					<option value="ALL">ALL</option>
+					<option value="male">Male</option>
+					<option value="female">Female</option>
+				</select>
+				<select
+					value={age}
+					onChange={(e) => setAge(e.target.value)}
+					style={{ marginLeft: '10px' }}
+				>
+					<option value="norm">Sort Age</option>
+					<option value="asc">Ascending</option>
+					<option value="desc">Descending</option>
+				</select>
+			</div>
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Name</th>
+						<th>Photo</th>
+						<th>Age</th>
+						<th>Gender</th>
+						<th>Company</th>
+						<th>Title</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					{sortedUsers.length > 0 ? (
+						sortedUsers.map((user) => (
+							<tr key={user.id} className="table-hover">
+								<td>{user.id}</td>
+								<td>
+									{user.firstName} {user.lastName}
+								</td>
+								<td>PHOTO</td>
+								<td>{user.age}</td>
+								<td>{genderMask(user.gender)}</td>
+								<td>{user.company.name}</td>
+								<td>{user.company.title}</td>
+								<td>
+									<button onClick={() => delUser(user.id)}>X</button>
+								</td>
+							</tr>
+						))
+					) : (
+						<tr>
+							<td colSpan="8">NOT FOUND!!!</td>
+						</tr>
+					)}
+				</tbody>
+			</table>
 		</Container>
 	);
 };
